bd42a3473645255a9bd62fed2bde4f2a9b9016c7
toayush <156430753+toayush@users.noreply.github.com>
2024-01-13 04:30:12 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553;
+let accountEmail = "ayush@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+// Uncommenting the next line will result in an error
+// accountId = 2;
+
+console.log(accountId);
